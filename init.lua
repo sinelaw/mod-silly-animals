f2d5ca46d304ead2045e7f1d03010ecd5acf5017
Noam Lewis <noam.lewis@elastifile.com>
2019-02-15 14:06:42 +0200

    unicorn

--- init.lua
+++ init.lua
@@ -20,3 +20,22 @@ minetest.register_node("silly_animals:panda",
                              fixed = {-1.25, -0.5, -0.5, 1, 1, 0.5}
                           },
 })
+
+
+minetest.register_node("silly_animals:unicorn",
+                       {
+                          description = "Unicorn Bear",
+                          drawtype = "mesh",
+                          mesh = "unicorn.obj",
+                          collision_box = {type = "fixed", fixed = {-1.25, -0.5, -0.5, 1, 1, 0.5}},
+                          light_source = 10,
+                          inventory_image = "unicorn-inv.png",
+                          paramtype = "light",
+                          paramtype2 = "facedir",
+                          tiles = {"unicorn-texture.png"},
+                          groups = {snappy=3},
+                          selection_box = {
+                             type = "fixed",
+                             fixed = {-1.25, -0.5, -0.5, 1, 1, 0.5}
+                          },
+})
